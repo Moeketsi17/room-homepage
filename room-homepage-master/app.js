a3b7074ffba3d9675e85754b42b6d9ee5bd8ef7a
Moeketsi17 <89091903+Moeketsi17@users.noreply.github.com>
2021-09-09 16:59:46 +0200

    Add files via upload

---
+++ room-homepage-master/app.js
@@ -0,0 +1,36 @@
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    
+
+    burger.addEventListener('click', ()=> {
+            // Toggle nav
+        nav.classList.toggle('nav-active');
+
+            /* Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            };
+        }); */
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+
+       
+    });
+
+
+}
+
+navSlide();
+
+
+
+
+
+
+
+
